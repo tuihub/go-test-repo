70e9323a0bc2ea388a5a39cf0e79a059d7e51f06
muzhou233 <MuZhou233@outlook.com>
2023-10-04 15:10:04 +0000

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module go-test-repo
 
 go 1.20
 
-require gopkg.in/yaml.v3 v3.0.0
+require github.com/ThreeDotsLabs/watermill v1.3.4
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,55 +1,34 @@
 package main
 
 import (
-        "fmt"
-        "log"
+	"context"
+	"fmt"
 
-        "gopkg.in/yaml.v3"
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
 )
 
-var data = `
-a: Easy!
-b:
-  c: 2
-  d: [3, 4]
-`
+func main() {
+	pubSub := gochannel.NewGoChannel(
+		gochannel.Config{},
+		watermill.NewStdLogger(false, false),
+	)
 
-// Note: struct fields must be public in order for unmarshal to
-// correctly populate the data.
-type T struct {
-        A string
-        B struct {
-                RenamedC int   `yaml:"c"`
-                D        []int `yaml:",flow"`
-        }
+	messages, err := pubSub.Subscribe(context.Background(), "example.topic")
+	if err != nil {
+		panic(err)
+	}
+
+	go process(messages)
 }
 
-func main() {
-        t := T{}
-    
-        err := yaml.Unmarshal([]byte(data), &t)
-        if err != nil {
-                log.Fatalf("error: %v", err)
-        }
-        fmt.Printf("--- t:\n%v\n\n", t)
-    
-        d, err := yaml.Marshal(&t)
-        if err != nil {
-                log.Fatalf("error: %v", err)
-        }
-        fmt.Printf("--- t dump:\n%s\n\n", string(d))
-    
-        m := make(map[interface{}]interface{})
-    
-        err = yaml.Unmarshal([]byte(data), &m)
-        if err != nil {
-                log.Fatalf("error: %v", err)
-        }
-        fmt.Printf("--- m:\n%v\n\n", m)
-    
-        d, err = yaml.Marshal(&m)
-        if err != nil {
-                log.Fatalf("error: %v", err)
-        }
-        fmt.Printf("--- m dump:\n%s\n\n", string(d))
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		fmt.Printf("received message: %s, payload: %s\n", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
 }
